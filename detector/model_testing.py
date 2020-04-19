fd766ae382da519fed3aa78965cf179e9c68a8e1
Shalin Patel <shalinpatel610@gmail.com>
2020-04-19 18:19:18 -0400

    confusion matrix done

--- detector/model_testing.py
+++ detector/model_testing.py
@@ -2,8 +2,9 @@ import os
 import re
 import math
 
-path = '/Users/shalin_patel/Desktop/Artificial Intelligence/Project/Project2/SpamDetector/test'
-
+'''
+Reading the model file and storing the ham and spam values
+'''
 modelValueHamDict = {}
 modelValueSpamDict = {}
 
@@ -14,6 +15,8 @@ for line in modelLines:
     modelValueLine = line.split()
     modelValueHamDict[modelValueLine[1]] = modelValueLine[3]
     modelValueSpamDict[modelValueLine[1]] = modelValueLine[5]
+
+path = '/Users/shalin_patel/Desktop/Artificial Intelligence/Project/Project2/SpamDetector/test'
 
 files = []
 
@@ -29,6 +32,15 @@ correctSpamCount = 0
 
 result = open("result.txt", "w")
 
+hamCorrect = 0
+hamNotCorrect = 0
+
+spamCorrect = 0
+spamNotCorrect = 0
+
+'''
+Reading all the test files and string the tokens in dict
+'''
 for r, d, f in os.walk(path):
     for file in f:
         if '.txt' in file:
@@ -46,6 +58,9 @@ for r, d, f in os.walk(path):
                 fileTokens = list(filter(lambda a: a != '', fileTokens))  # removing of blank spaces
                 dictSpamTestFiles[file] = fileTokens
 
+'''
+Evaluating each file ans saving the result for ham files
+'''
 for key, value in dictHamTestFiles.items():
     hamProbability = 0
     spamProbability = 0
@@ -57,10 +72,15 @@ for key, value in dictHamTestFiles.items():
     result.write(str(counter) + "  " + str(key) + "  ")
     if hamProbability > spamProbability:
         correctHamCount += 1
+        hamCorrect += 1
         result.write(str("ham  ") + str(hamProbability) + "  " + str(spamProbability) + "  ham  right\n")
     else:
+        hamNotCorrect += 1
         result.write(str("spam  ") + str(hamProbability) + "  " + str(spamProbability) + "  ham  wrong\n")
 
+'''
+Evaluating each file ans saving the result for spam files
+'''
 for key, value in dictSpamTestFiles.items():
     hamProbability = 0
     spamProbability = 0
@@ -72,9 +92,37 @@ for key, value in dictSpamTestFiles.items():
     result.write(str(counter) + "  " + str(key) + "  ")
     if spamProbability > hamProbability:
         correctSpamCount += 1
+        spamCorrect += 1
         result.write(str("spam  ") + str(hamProbability) + "  " + str(spamProbability) + "  spam  right\n")
     else:
+        spamNotCorrect += 1
         result.write(str("ham  ") + str(hamProbability) + "  " + str(spamProbability) + "  spam  wrong\n")
 
 result.close()
-print("Result File Created")
+print("\nResult File Created\n")
+
+'''
+Confusion Matrix - HAM
+'''
+print("CONFUSION MATRIX\n")
+print("          " + "HAM   " + "   SPAM   ")
+print("HAM   |   " + str(hamCorrect) + "   |   " + str(hamNotCorrect))
+print("SPAM  |   " + str(spamCorrect) + "   |   " + str(spamNotCorrect))
+
+print("\nHAM ACCURACY  - " + str(hamCorrect/(hamCorrect+hamNotCorrect)))
+print("SPAM ACCURACY - " + str(spamCorrect/(spamCorrect+spamNotCorrect)))
+
+hamRecall = hamCorrect / (hamCorrect + hamNotCorrect)
+spamRecall = spamCorrect / (spamCorrect + spamNotCorrect)
+print("\nHAM RECALL  - " + str(hamRecall))
+print("SPAM RECALL - " + str(spamRecall))
+
+hamPrecision = hamCorrect / (hamCorrect + spamCorrect)
+spamPrecision = spamCorrect / (spamCorrect + hamCorrect)
+print("\nHAM PRECISION  - " + str(hamPrecision))
+print("SPAM PRECISION - " + str(spamPrecision))
+
+hamFMeasure = (2*hamRecall*hamPrecision) / (hamRecall + hamPrecision)
+spamFMeasure = (2*spamRecall*spamPrecision) / (spamRecall + spamPrecision)
+print("\nHAM F-Measure  - " + str(hamFMeasure))
+print("SPAM F-Measure - " + str(spamFMeasure))
